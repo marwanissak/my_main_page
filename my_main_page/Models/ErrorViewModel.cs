4cdf9f3da4f7386c7f21384278466f9d8de25933
marwan <marwanissak@hotmail.com>
2019-07-28 18:54:08 +0200

    Add project files.

---
+++ my_main_page/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace my_main_page.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
