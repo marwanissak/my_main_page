839cb4d14723e5dfa2182131644ed2191e99b747
marwan <marwanissak@hotmail.com>
2019-07-28 23:10:06 +0200

    Redid some of the html text

--- my_main_page/Controllers/HomeController.cs
+++ my_main_page/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace my_main_page.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "My projects:";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "My contact page.";
 
             return View();
         }
